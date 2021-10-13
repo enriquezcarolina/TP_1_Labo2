2b6896225b8871dde5fd2c15c8d62252dd43ff44
Caterina <66692523+Categalafassi@users.noreply.github.com>
2021-10-13 11:39:49 -0300

    Funciones atacar

--- TP_1_Labo2/Class1.cs
+++ TP_1_Labo2/Class1.cs
@@ -18,20 +18,128 @@ namespace TP_1_Labo2
         
     }
 
-
-    public class pieza
-    { 
-
-        public virtual void atacar(tablero tablero);  
-
-    }
-   
-    public class reina : pieza
+    public class Pieza
     {
-
-        public override void atacar(tablero tablero)
+        public int[] Pos = new int[2];
+        public virtual void Atacar(Tablero tablero)
         {
 
+        }
+    }
+   
+    public class Torre : Pieza
+    {
+        public override void Atacar(Tablero tablero)
+        {
+            for (int i = 0; i < constantes.TAM; i++) //ataca toda la fila
+            {
+                if(i!=Pos[0]) //que no ataque su posicion
+                    tablero.atacadas[i, Pos[1]] = constantes.ATACADA;
+            }
+            for(int j=0; j<constantes.TAM; j++) //ataca toda la columna
+            {
+                if (j != Pos[1]) //que no ataque su posicion
+                    tablero.atacadas[Pos[0], j] = constantes.ATACADA;
+            }
+        }
+    }
+
+    public class Caballo : Pieza
+    {
+        public override void Atacar (Tablero tablero)
+        {
+            //no se como hacerlo con un for entonces puse que casilleros ataca uno por uno
+            tablero.atacadas[Pos[0] + 2, Pos[1] + 1] = constantes.ATACADA;
+            tablero.atacadas[Pos[0] + 2, Pos[1] - 1] = constantes.ATACADA;
+            tablero.atacadas[Pos[0] - 2, Pos[1] + 1] = constantes.ATACADA;
+            tablero.atacadas[Pos[0] - 2, Pos[1] - 1] = constantes.ATACADA;
+            tablero.atacadas[Pos[0] + 1, Pos[1] + 2] = constantes.ATACADA;
+            tablero.atacadas[Pos[0] - 1, Pos[1] + 2] = constantes.ATACADA;
+            tablero.atacadas[Pos[0] + 1, Pos[1] - 2] = constantes.ATACADA;
+            tablero.atacadas[Pos[0] - 1, Pos[1] - 2] = constantes.ATACADA;
+        }
+    }
+
+    public class Alfil : Pieza
+    {
+
+        public override void Atacar(Tablero tablero)
+        {
+            for(int i = 1; i < constantes.TAM; i++)//desde 1 porque con 0 atacaria su posicion
+            { 
+                //el alfil ataca en diagonal entonces sumo o resto lo mismo a la fila y la columa
+
+                if (Pos[0] + i < constantes.TAM && Pos[1] - i >= 0) //chequeo que no se pase del tablero
+                    tablero.atacadas[Pos[0] + i, Pos[1] - i] = constantes.ATACADA;
+               
+                if (Pos[0] - i >=0 && Pos[1] + i < constantes.TAM) 
+                    tablero.atacadas[Pos[0] - i, Pos[1] + i] = constantes.ATACADA;
+               
+               
+                if(Pos[0] + i < constantes.TAM && Pos[1] + i < constantes.TAM)
+                    tablero.atacadas[Pos[0] + i, Pos[1] + i] = constantes.ATACADA;
+               
+                if (Pos[0] - i >= 0 && Pos[1] - i >= 0)
+                    tablero.atacadas[Pos[0] - i, Pos[1] - i] = constantes.ATACADA;
+            }
+        }
+    }
+
+    public class Rey : Pieza
+    {
+
+        public override void Atacar(Tablero tablero)
+        {
+            for(int i=-1; i<2; i++)
+            {
+                for(int j=-1; j<2; j++)
+                {
+                    if(!(i==0 && j==0))//que no ataque su posicion
+                    {
+                        tablero.atacadas[Pos[0] + i, Pos[1] + j] = constantes.ATACADA;
+                    }
+                }
+            }
+        }
+    }
+
+    public class Reina : Pieza
+    {
+
+        public override void Atacar(Tablero tablero)
+        {
+            //ataque alfil + ataque torre
+
+            //ALFIL
+            for (int i = 1; i < constantes.TAM; i++)//desde 1 porque con 0 atacaria su posicion
+            {
+                //el alfil ataca en diagonal entonces sumo o resto lo mismo a la fila y la columa
+
+                if (Pos[0] + i < constantes.TAM && Pos[1] - i >= 0) //chequeo que no se pase del tablero
+                    tablero.atacadas[Pos[0] + i, Pos[1] - i] = constantes.ATACADA;
+
+                if (Pos[0] - i >= 0 && Pos[1] + i < constantes.TAM)
+                    tablero.atacadas[Pos[0] - i, Pos[1] + i] = constantes.ATACADA;
+
+
+                if (Pos[0] + i < constantes.TAM && Pos[1] + i < constantes.TAM)
+                    tablero.atacadas[Pos[0] + i, Pos[1] + i] = constantes.ATACADA;
+
+                if (Pos[0] - i >= 0 && Pos[1] - i >= 0)
+                    tablero.atacadas[Pos[0] - i, Pos[1] - i] = constantes.ATACADA;
+            }
+
+            //TORRE
+            for (int i = 0; i < constantes.TAM; i++) //ataca toda la fila
+            {
+                if (i != Pos[0]) //que no ataque su posicion
+                    tablero.atacadas[i, Pos[1]] = constantes.ATACADA;
+            }
+            for (int j = 0; j < constantes.TAM; j++) //ataca toda la columna
+            {
+                if (j != Pos[1]) //que no ataque su posicion
+                    tablero.atacadas[Pos[0], j] = constantes.ATACADA;
+            }
         }
     }
 
@@ -71,8 +179,3 @@ namespace TP_1_Labo2
 
 
 }
-
-            
-         
-
-
