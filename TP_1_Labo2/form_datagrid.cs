1370c0857de4e669fe07f1a7ff30ac48434d0c31
Caterina <66692523+Categalafassi@users.noreply.github.com>
2021-11-04 15:03:58 -0300

    comentarios

--- TP_1_Labo2/form_datagrid.cs
+++ TP_1_Labo2/form_datagrid.cs
@@ -13,22 +13,22 @@ namespace TP_1_Labo2
     public partial class form_datagrid : Form
     {
 
-        private Pieza[,] Solucion = new Pieza[constantes.TAM, constantes.TAM];
-        private List<Tablero> Soluciones_ = new List<Tablero>();
+        private Pieza[,] Solucion = new Pieza[constantes.TAM, constantes.TAM]; //matriz de piezas para posicionarlas donde van en la solucion
+        private List<Tablero> Soluciones_ = new List<Tablero>(); //se guarda la lista de soluciones
 
         public form_datagrid(List<Tablero> soluciones)
         {
             Soluciones_ = soluciones;
 
             InitializeComponent();
-            DataGridView.RowCount = constantes.TAM;
+            DataGridView.RowCount = constantes.TAM; //grid del tamaño del tablero (8x8)
             DataGridView.ColumnCount = constantes.TAM;
-            next();
+            next(); //imprimo la primera solucion
         }
 
         private void DataGridView_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
-        {
-
+        {   //formato de la celdas para que sea un tablero
+            //cuadros intercalados blanco y negro
             if (e.RowIndex % 2 == 0 && e.ColumnIndex % 2 == 0)
             {
                 e.CellStyle.BackColor = Color.Black;
@@ -44,36 +44,36 @@ namespace TP_1_Labo2
             DataGridView dgv = sender as DataGridView;
             for (int i = 0; i < DataGridView.ColumnCount; i++)
             {
-                dgv.Columns[i].Width = 50;
+                dgv.Columns[i].Width = 50; //tamaño de las columnas para que sea un cuadrado
             }
 
         }
 
-        private int cont = 0;
+        private int cont = 0; //contador para ir imprimiendo las soluciones
+        
         private void buttonNext_Click_1(object sender, EventArgs e)
         {   
-            if (cont == Soluciones_.Count()-1)
+            if (cont == Soluciones_.Count()-1) //ver si ya llego a la ultima
                 this.Close();
-            next();
-            
-          
+            next(); //imprimo la solucion             
         }
 
         public void next()
         {
-            textBox1.Text = "Solucion : " + (cont + 1);
+            textBox1.Text = "Solucion : " + (cont + 1); //que numero de solucion va
+          
             for (int i = 0; i < constantes.TAM; i++)
             {
                 for (int j = 0; i < constantes.TAM; i++)
                 {
-                    Solucion[i, j] = null;
+                    Solucion[i, j] = null; //reseteo la matriz de piezas a null para posicionar las de la solucion que quiero mostrar
                 }
             }
 
             int[] pos;
             for (int i = 0; i < constantes.CANT_PIEZAS; i++)
-            {
-                pos = Soluciones_[cont].piezas[i].Pos;
+            {    //voy pieza por pieza(i) en la solucion que estoy(cont) y las posiciono en la matriz solucion en su posicion
+                pos = Soluciones_[cont].piezas[i].Pos; 
                 Solucion[pos[0], pos[1]] = Soluciones_[cont].piezas[i];
             }
 
@@ -82,44 +82,36 @@ namespace TP_1_Labo2
                 for (int j = 0; i < DataGridView.ColumnCount; i++)
                 {
                     if (Solucion[i, j] != null)
-                    {
+                    { //recorro toda la matriz solucion y en las posiciones en las que hay una pieza imprimo el nombre en el DataGrid
                         DataGridView[i, j].Value = Solucion[i, j].nombre;
 
                     }
                 }
             }
-            cont++;
+            cont++; //paso a la siguiente
         }
 
 
-        //IGNORAR
+        // IGNORAR
 
         private void buttonNext_Click(object sender, EventArgs e)
         {
-
         }
 
-
         private void form_datagrid_Load(object sender, EventArgs e)
-        {
-            
+        {           
         }
 
         private void tableroDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
         }
 
         private void tableroBindingSource_CurrentChanged(object sender, EventArgs e)
         {
-
         }
 
         private void form_datagrid_FormClosing(object sender, FormClosingEventArgs e)
         {
-
         }
-
-      
     }
 }
